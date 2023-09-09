4139b092a46a7df60eb0ad842238501b61db60a5
anonymous <101496703+themelody77@users.noreply.github.com>
2023-09-09 15:45:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,28 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify
 import pickle
 app = Flask(__name__)
 
-with open('biogas_model', 'rb') as f:
+with open('biogas_model','rb') as f:
     __model = pickle.load(f)
-
-@app.route('/getprediction', methods=['POST'])
+ 
+@app.route('/getprediction',methods=['POST'])
 def get_biogas_prediction():
     x = request.json.get('food_waste')
     if float(x) == 0:
         response = jsonify({
-            'estimation': 0
+            'estimation' : 0
         })
     else:
         response = jsonify({
-            'estimation': round(__model.predict([[float(x)]])[0], 2)
+            'estimation' : round(__model.predict([[float(x)]])[0],2)
         })
-    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Origin','*')
     return response
 
-@app.route('/', methods=['GET'])
-def homepage():
-    return "I am on the web page"
+@app.route("/",methods=['GET'])
+def hello():
+    return "hi"
+
 
 if __name__ == "__main__":
     print("Running Python server for Biogas prediction")
