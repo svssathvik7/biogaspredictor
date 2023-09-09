c9de55fc9c27692bcae3ca793c1d31a160d874ca
anonymous <101496703+themelody77@users.noreply.github.com>
2023-09-09 13:35:59 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request, jsonify
+import pickle
+app = Flask(__name__)
+
+with open('biogas_model','rb') as f:
+    __model = pickle.load(f)
+ 
+@app.route('/getprediction',methods=['POST'])
+def get_biogas_prediction():
+    x = request.json.get('food_waste')
+    if float(x) == 0:
+        response = jsonify({
+            'estimation' : 0
+        })
+    else:
+        response = jsonify({
+            'estimation' : round(__model.predict([[float(x)]])[0],2)
+        })
+    response.headers.add('Access-Control-Allow-Origin','*')
+    return response
+
+
+if __name__ == "__main__":
+    print("Running Python server for Biogas prediction")
+    app.run()
